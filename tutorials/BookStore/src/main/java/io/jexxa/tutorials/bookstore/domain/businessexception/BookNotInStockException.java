b400b64e818a742cc0b4a1b922aa7b31f9da98b8
di39258 <tobias.bodo@dillinger.biz>
2020-08-17 09:35:40 +0200

    hinzufuegen Code des Interfaces IBookRepository und IDomainEventPublisher , soiwe der Klassen ISBN13 und BookSoldOut

--- tutorials/BookStore/src/main/java/io/jexxa/tutorials/bookstore/domain/businessexception/BookNotInStockException.java
+++ tutorials/BookStore/src/main/java/io/jexxa/tutorials/bookstore/domain/businessexception/BookNotInStockException.java
@@ -1,5 +1,9 @@
 package io.jexxa.tutorials.bookstore.domain.businessexception;
 
-public class BookNotInStockException
+/**
+ * Is thrown in case we try to sell a book that is currently not in stock
+ */
+public class BookNotInStockException extends Exception
 {
+    private static final long serialVersionUID = 1L;
 }
