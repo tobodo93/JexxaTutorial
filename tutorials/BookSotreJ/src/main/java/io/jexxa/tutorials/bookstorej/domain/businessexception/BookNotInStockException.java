46b00406b826e2466f07646aad6017708e0eac47
di39258 <tobias.bodo@dillinger.biz>
2020-08-21 13:47:16 +0200

    hinzufuegen Code für domain Book, BookNotInStockException, BookSoldOut und ISBN13

--- tutorials/BookSotreJ/src/main/java/io/jexxa/tutorials/bookstorej/domain/businessexception/BookNotInStockException.java
+++ tutorials/BookSotreJ/src/main/java/io/jexxa/tutorials/bookstorej/domain/businessexception/BookNotInStockException.java
@@ -1,5 +1,12 @@
 package io.jexxa.tutorials.bookstorej.domain.businessexception;
 
-public class BookNotInStockException
+import io.jexxa.addend.applicationcore.BusinessException;
+
+/**
+ * Is thrown in case we try to sell a book that is currently not in stock
+ */
+@BusinessException
+public class BookNotInStockException extends Exception
 {
+    private static final long serialVersionUID = 1L;
 }
--- tutorials/BookSotreJ/src/main/java/io/jexxa/tutorials/bookstorej/domain/domainevent/BookSoldOut.java
+++ tutorials/BookSotreJ/src/main/java/io/jexxa/tutorials/bookstorej/domain/domainevent/BookSoldOut.java
@@ -1,5 +1,25 @@
 package io.jexxa.tutorials.bookstorej.domain.domainevent;
 
-public class BookSoldOut
+import io.jexxa.addend.applicationcore.DomainEvent;
+import io.jexxa.tutorials.bookstorej.domain.valueobject.ISBN13;
+
+@DomainEvent("BookSoldOut")
+public final class BookSoldOut
 {
+    private final ISBN13 isbn13;
+
+    private BookSoldOut(ISBN13 isbn13)
+    {
+        this.isbn13 = isbn13;
+    }
+
+    public ISBN13 getISBN13()
+    {
+        return isbn13;
+    }
+
+    public static BookSoldOut bookSoldOut(ISBN13 isbn13)
+    {
+        return new BookSoldOut(isbn13);
+    }
 }
